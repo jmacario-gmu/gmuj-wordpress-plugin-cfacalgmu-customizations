1524b99a77d168bb5e6be95007b8af66301c7e22
Jan Macario <jmacario@gmu.edu>
2022-07-29 09:49:22 -0400

    Modifies a hrefs for site logo and title to point to main CFA site.

--- js/cfacalgmu-custom.js
+++ js/cfacalgmu-custom.js
@@ -5,4 +5,7 @@ jQuery(document).ready(function(){
 	// Debug
 	//alert('custom js file loaded');
 
+	// Modify href of site logo/title to go to main Hylton site
+	jQuery(".header-titles a").prop("href", "https://cfa.gmu.edu")
+
 });
