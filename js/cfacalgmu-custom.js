d33f3b1578ffd18480e846d24c02a7e903001493
Jan Macario <jmacario@gmu.edu>
2025-04-02 16:35:19 -0400

    Modifies site title to include a line break via jQuery, which we can't do in the WordPress theme customizer.

--- js/cfacalgmu-custom.js
+++ js/cfacalgmu-custom.js
@@ -8,4 +8,7 @@ jQuery(document).ready(function(){
 	// Modify href of site logo/title to go to main Hylton site
 	jQuery(".site-logo a").prop("href", "https://cfa.gmu.edu")
 
+	// Modify site title to be on two lines
+	jQuery(".site-name a").html("CENTER FOR THE ARTS<br />EVENT CALENDAR")
+
 });
