5b1baa17c9082e976838bcd6096d168185ece234
Jan Macario <jmacario@gmu.edu>
2026-06-25 09:59:52 -0400

    Updates JS file to fix site logo/title/home link hrefs.

--- js/cfacalgmu-custom.js
+++ js/cfacalgmu-custom.js
@@ -5,8 +5,14 @@ jQuery(document).ready(function(){
 	// Debug
 	//alert('custom js file loaded');
 
-	// Modify href of site logo/title to go to main Hylton site
-	jQuery(".site-logo a").prop("href", "https://cfa.gmu.edu")
+	// Modify href of site logo to go to main CFA site
+	jQuery(".header-logo a").prop("href", "https://cfa.gmu.edu")
+
+	// Modify href of site title to go to main CFA site
+	jQuery(".site-name a").prop("href", "https://cfa.gmu.edu")
+
+	// Modify href of main nav home link to go to main CFA site
+	jQuery(".gmuw_main_nav_home a").prop("href", "https://cfa.gmu.edu")
 
 	// Modify site title to be on two lines
 	jQuery(".site-name a").html("CENTER FOR THE ARTS<br />EVENT CALENDAR")
